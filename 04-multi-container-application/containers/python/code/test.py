e50b6602be646eaa2537f1d2bbbd3c6b2ea9ba1a
Andrew Mcshane <andrew.mcshane@valtech.com>
2019-08-13 12:41:41 +0100

    semi-working config for a multi-container application, needs deploying/testing/cleaning up

---
+++ 04-multi-container-application/containers/python/code/test.py
@@ -0,0 +1,30 @@
+#!/usr/bin/env python3
+
+import MySQLdb
+from flask import Flask
+app = Flask(__name__)
+
+@app.route("/")
+def hello():
+    return "Hello World!"
+
+@app.route("/something")
+def something():
+    return "Were you looking for something?"
+
+@app.route("/db")
+def databasetest():
+
+    db = MySQLdb.connect(host="mariadb-clusterip.default.svc.cluster.local",
+                        user="test_user", 
+                        passwd="test_pass",
+                        db="test_db") 
+    if db:
+        db_output = "because the database connection works!"
+    else:
+        db_output = "db connection broke"
+
+    return db_output
+
+if __name__ == "__main__":
+    app.run(host='0.0.0.0', port=80)
